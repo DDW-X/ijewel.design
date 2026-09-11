0e63080c2e55b26ebca6584fd199b28cb0c5322d
DDW-X <ml3740965@gmail.com>
2026-09-12 01:54:13 +0330

    DDW-X

---
+++ _next/static/chunks/pages/pricing-b7e97def024321e5.js
@@ -0,0 +1,11 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9939],{6915:function(n,t,e){"use strict";e.d(t,{Z:function(){return u}});/**
+ * @license lucide-react v0.424.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let u=(0,e(1373).Z)("Copy",[["rect",{width:"14",height:"14",x:"8",y:"8",rx:"2",ry:"2",key:"17jyea"}],["path",{d:"M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2",key:"zix9uf"}]])},1352:function(n,t,e){"use strict";e.d(t,{Z:function(){return u}});/**
+ * @license lucide-react v0.424.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let u=(0,e(1373).Z)("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]])},2691:function(n,t,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/pricing",function(){return e(3054)}])}},function(n){n.O(0,[1481,8121,2215,3858,674,4863,1821,6172,5805,3527,8308,683,3054,2888,9774,179],function(){return n(n.s=2691)}),_N_E=n.O()}]);
